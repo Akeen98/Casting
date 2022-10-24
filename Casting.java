b1330b86ef8852f34085899fcc4ac6e73c0069f2
Akeen Bnadara <akeenbandara9@gmail.com>
2022-10-24 22:48:47 +0530

    crete casting example in java

---
+++ Casting.java
@@ -0,0 +1,11 @@
+public class Casting{
+    public static void main(String []args){
+        int x,y;
+        x=12;
+        y=21;
+        double z = (double)(x+y)/2;
+        System.out.println(z);
+
+
+    }
+}
